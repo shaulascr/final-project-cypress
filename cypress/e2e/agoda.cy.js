5db436dfc5b888690200dd72a2ca5f4ba9d60890
shaulascr <alya.roosyidah@gmail.com>
2025-06-19 01:53:07 +0700

    fix agoda

--- cypress/e2e/agoda.cy.js
+++ cypress/e2e/agoda.cy.js
@@ -19,30 +19,3 @@ describe('Memilih tiket pesawat tujuan', function() {
     agodaPage.filterAirlines(this.data.selectAirlines);
   });
 });
-
- // cy.visit(Cypress.env('agodaBaseUrl'));
-    // // cy.get('.tab-flight-tab').click();
-    // cy.get('[data-selenium="agodaFlightsTab"]').contains('Flights').click()
-    // cy.url().should('include', '#flights');
-    // cy.wait(1000); 
-    // cy.get('[data-selenium="flight-origin-search-input"]')
-    //   .should('be.visible')
-    //   .clear()
-    //   .type('Jakarta');
-    // cy.wait(1000); 
-    // cy.get('[data-selenium="suggestion-text-highlight"]').should('have.text', 'Jakarta')
-    // cy.contains('Soekarno-Hatta International Airport').click();
-    // cy.get('#flight-origin-search-input')
-    //   .should('have.value', 'Jakarta (CGK)');
-    // cy.get('[data-selenium="flight-destination-search-input"]')
-    // .should('be.visible')
-    // .clear()
-    // .type('Singapura');
-    // cy.wait(1000);
-    // cy.get('[data-element-index="0"]')
-    //   .should('be.visible')
-    //   .and('contain.text', 'Singapore, Singapore');
-    // // cy.get('[data-selenium="suggestion-text"]').should('have.text', 'Singapore, Singapore');
-    // // data-selenium="suggestion-text-highlight"
-    // // cy.contains('Soekarno-Hatta International Airport').click();
-    // //  Changi Airport
